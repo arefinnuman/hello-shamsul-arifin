4e8c7aa83ed1450e65d07c6db2c58a06c22f98b7
arefinnuman <arefinnuman2000@gmail.com>
2022-12-12 00:55:18 +0600

    Margin Added

--- src/Pages/Home/Projects.jsx
+++ src/Pages/Home/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
   return (
     <div className="mb-10" id="projects">
       <div className="text-center mt-20">
-        <h1 className="text-4xl font-bold">Some Recent Projects</h1>
+        <h1 className="text-3xl font-bold mb-10">Some Recent Projects</h1>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-5">
