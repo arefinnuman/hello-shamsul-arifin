633e289e9bb805d2638e3983c4710d03509b503c
arefinnuman <arefinnuman2000@gmail.com>
2022-12-24 03:25:08 +0600

    Spinner Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import RingLoader from "react-spinners/RingLoader";
 import { router } from "./Routes/Public/Routes";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
     <>
-      <RouterProvider router={router} />
+      {
+        // If loading is true, show loading screen
+        loading ? (
+          <div className="flex items-center justify-center min-h-screen">
+            <div>
+              <RingLoader
+                color={"#007CED"}
+                loading={loading}
+                size={30}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </div>
+          </div>
+        ) : (
+          <RouterProvider router={router} />
+        )
+      }
     </>
   );
 }
